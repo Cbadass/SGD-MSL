10384b40868b703030e104888daa903728e8d84f
Cbadass <sebastian2000morales@gmail.com>
2025-06-02 02:16:49 -0400

    Add session check: redirect to login if user is not authenticated

--- index.php
+++ index.php
@@ -1,5 +1,9 @@
 <?php
 session_start();
+if (!isset($_SESSION['usuario'])) {
+    header("Location: login.php");
+    exit;
+}
 $seccion = $_GET['seccion'] ?? 'usuarios';
 $modo_oscuro = $_COOKIE['modo_oscuro'] ?? 'false';
 ?>
