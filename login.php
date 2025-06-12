01feec92304b39f58ed53e81e78e78f96531390f
Zipelbrum <a.montero.g98@gmail.com>
2025-06-11 21:17:59 -0400

    Cambio "Inicio"

--- login.php
+++ login.php
@@ -46,7 +46,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <html lang="es">
 <head>
 <meta charset="UTF-8">
-<title>Iniciar Sesión</title>
+<title>Iniciar</title>
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body class="container mt-5">
