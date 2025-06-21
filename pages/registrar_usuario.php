ee80c6e971f7aaa6977b8b830998af56f2dc7273
Zipelbrum <a.montero.g98@gmail.com>
2025-06-20 21:40:48 -0400

    V2

--- pages/registrar_usuario.php
+++ pages/registrar_usuario.php
@@ -231,7 +231,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 <head>
   <meta charset="UTF-8">
   <title>Registrar Profesional</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
   <link rel="stylesheet" href="style.css">
 </head>
 
