858fdd0015961652b31604d81d6e13dc68b8f908
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-06-23 19:25:42 -0400

    header pt.3

--- components/header.php
+++ components/header.php
@@ -7,7 +7,7 @@ if (!isset($_SESSION['usuario'])) {
 $usuario = $_SESSION['usuario'];
 ?>
 <header class="header">
-  <span>SGD Multisenluz</span>
+  <span class="header-title">SGD Multisenluz</span>
   <div>
     <span style="mr-1">
       <?= htmlspecialchars($usuario['nombre']) ?>
@@ -16,7 +16,7 @@ $usuario = $_SESSION['usuario'];
     <button class="btn btn-sm btn-outline-light mr-1">
       <a class="link-text" href="index.php?seccion=perfil">👤 Mi Perfil</a>
     </button>
-    <button id="modoToggle" class="btn btn-sm btn-light toggle-darkmode">🌙</button>
+    <button id="modoToggle" class="btn btn-sm btn-light toggle-darkmode mr-1">🌙</button>
     <button  class="btn btn-sm btn-outline-light">
       <a class="link-text" href="logout.php">Cerrar sesión</a>
     </button>
