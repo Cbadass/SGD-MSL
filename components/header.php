22a23a1615804e5273cd14f27a0d43a96b62793f
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-06-23 19:16:11 -0400

    header pt.2

--- components/header.php
+++ components/header.php
@@ -1,22 +1,24 @@
 <?php
 session_start();
 if (!isset($_SESSION['usuario'])) {
-    header("Location: login.php");
-    exit;
+  header("Location: login.php");
+  exit;
 }
 $usuario = $_SESSION['usuario'];
 ?>
 <header class="header">
   <span>SGD Multisenluz</span>
   <div>
-    <button class="btn btn-sm btn-outline-light me-2">
-      <a class="link-text" href="index.php?seccion=perfil">👤 Mi Perfil</a>
-    </button>
-    <span>
-      Usuario: <strong><?= htmlspecialchars($usuario['nombre']) ?></strong>
+    <span style="mr-1">
+      <?= htmlspecialchars($usuario['nombre']) ?>
       (<?= htmlspecialchars($usuario['permisos']) ?>)
     </span>
-    <button id="modoToggle" class="btn btn-sm btn-light me-2 toggle-darkmode">🌙</button>
-    <a href="logout.php" class="btn btn-sm btn-outline-light">Cerrar sesión</a>
+    <button class="btn btn-sm btn-outline-light mr-1">
+      <a class="link-text" href="index.php?seccion=perfil">👤 Mi Perfil</a>
+    </button>
+    <button id="modoToggle" class="btn btn-sm btn-light toggle-darkmode">🌙</button>
+    <button  class="btn btn-sm btn-outline-light">
+      <a class="link-text" href="logout.php">Cerrar sesión</a>
+    </button>
   </div>
 </header>
