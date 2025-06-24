01c83b1da4474bdad715c596a488369872c45347
lucio-crDEV <lucio.m.cardenas@gmail.com>
2025-06-23 20:56:33 -0400

    fix palabra repetida

--- pages/usuarios.php
+++ pages/usuarios.php
@@ -150,7 +150,7 @@ if ($usuarios) {
                 <td>".htmlspecialchars($row['Nombre_escuela']     ?? 'Otra')."</td>
                 <td>".htmlspecialchars($row['Permisos']           ?? 'user')."</td>
                 <td>".($row['Estado_usuario']==1 ? 'Activo':'Inactivo')."</td>
-                <td style='display:flex; justify-content:space-space-around;'>
+                <td style='display:flex; justify-content:space-around;'>
                     <a href='index.php?seccion=modificar_profesional&Id_profesional=" . htmlspecialchars($row['Id_profesional']) . "' class='btn btn-sm btn-warning me-1 link-text'>Editar</a>
                     <a href='index.php?seccion=documentos&id_prof=" . htmlspecialchars($row['Id_profesional']) . "&sin_estudiante=1' class='btn btn-sm btn-info link-text'>Docs libres</a>
                     <a href=\"index.php?seccion=perfil&Id_profesional={$row['Id_profesional']}\"
